d0eb8b92e69a00f61223087f4ee290c43bc5f99d
mikalikahi <sweeney.phil@gmail.com>
2023-01-28 16:06:14 +0800

    Basic frontend microservice structure

---
+++ MicroservicesFrontEnd/src/main/java/MicroservicesFrontEnd/MicroservicesFrontEnd/MicroservicesFrontEndApplication.java
@@ -0,0 +1,13 @@
+package MicroservicesFrontEnd.MicroservicesFrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesFrontEndApplication.class, args);
+	}
+
+}
---
+++ MicroservicesFrontEnd/src/test/java/MicroservicesFrontEnd/MicroservicesFrontEnd/MicroservicesFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroservicesFrontEnd.MicroservicesFrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
